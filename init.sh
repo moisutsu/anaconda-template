2a0d2ea079ca3debf0a3193fedaf4e6e4f6235da
moisutsu <moisutsu@gmail.com>
2022-06-01 09:56:32 +0900

    Add --no-builds option to conda env export

--- init.sh
+++ init.sh
@@ -44,7 +44,7 @@ direnv allow
 
 # Create conda env file
 source .envrc
-conda env export > "$CONDA_ENV_FILE"
+conda env export --no-builds > "$CONDA_ENV_FILE"
 
 # Use pip in conda
 conda config --set pip_interop_enabled True
